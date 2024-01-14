fef032ddad3ba0975685e0aa064bbe73987bdf1a
only1tarunno <tarunno281@gmail.com>
2024-01-14 13:18:51 +0600

    add component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
+import Banner from "./components/Banner";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import TaskBoard from "./components/Tasks/TaskBoard";
+
 function App() {
   return (
     <>
-      <h1>helllo</h1>
+      <Header />
+      <Banner />
+      <TaskBoard />
+      <Footer />
     </>
   );
 }
